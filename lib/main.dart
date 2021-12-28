869dfa6105e53cd34f4e6cbe670f1653a8f2e80d
Prabhneet Narula <imailcode@gmail.com>
2021-12-29 01:56:03 +0530

    day1 changes done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/front_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      routes: {
+        "/": (context) => const FrontPage(),
+      },
+    );
   }
 }
-  
