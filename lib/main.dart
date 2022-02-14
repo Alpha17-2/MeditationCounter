1904d873121152f93356cf11f21c4e4248d021fc
Prabhneet Narula <imailcode@gmail.com>
2022-02-14 22:55:37 +0530

    Some Major Changes Done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,16 @@
-import 'package:counter/pages/counter_page.dart';
-import 'package:counter/pages/login_page.dart';
-import 'package:counter/utils/routes.dart';
+import 'package:counter/pages/firstPage.dart';
 import 'package:flutter/material.dart';
-import './pages/front_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      routes: {
-        "/": (context) => const FrontPage(),
-        MyRoutes.loginRoute: (context) => const LoginPage(),
-        MyRoutes.counterRoute: (context) => const CounterPage(),
-      },
+      home: FirstPage(),
     );
   }
 }
