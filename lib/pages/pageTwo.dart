f78b9c8b917b63dbf5b3abe5ae6b7f2ebed96f66
Alpha17-2 <alphaisgod1@gmail.com>
2022-02-25 22:52:35 +0530

    patch : introduced scroll view for the items in login screen

--- lib/pages/pageTwo.dart
+++ lib/pages/pageTwo.dart
@@ -36,135 +36,136 @@ class _PageTwoState extends State<PageTwo> {
         backgroundColor: Colors.grey.shade200,
         body: SafeArea(
             child: Container(
-          width: double.infinity,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: deviceHeight * 0.09),
-                child: Lottie.asset(
-                    //Lottie Animation
-                    "lib/assets/images/signIn.json"),
-              ),
-              //SizedBox
-              const SizedBox(height: 14),
-              //Welcome Message
-              Text("Welcome",
-                  style: GoogleFonts.cagliostro(
-                      fontSize: MediaQuery.of(context)
-                          .textScaleFactor
-                          .clamp(deviceWidth * 0.101, deviceWidth * 0.2),
-                      fontWeight: FontWeight.w600)),
-              //SizedBox
-              Flexible(child: Container(), flex: 1),
-              //InputFormField for email
-              TextFieldInput(
-                  textEditingController: _emailController,
-                  hintText: "Enter your Username",
-                  labelText: "Username",
-                  textInputType: TextInputType.emailAddress),
-              //SizedBox
-              const SizedBox(
-                height: 20,
-              ),
-              //InputFormfield for password
-              TextFieldInput(
-                textEditingController: _passwordController,
-                hintText: "Enter your Password",
-                labelText: "Password",
-                textInputType: TextInputType.text,
-                isPass: true,
-              ),
-              //FlexibleWidget
-              Flexible(child: Container(), flex: 2),
-              //Login Button
-              GestureDetector(
-                onTap: () async {
-                  setState(() {
-                    _isLoading = true;
-                    _isElevated = !_isElevated;
-                  });
-                  await Future.delayed(const Duration(milliseconds: 400));
-                  String result = await AuthMethods().loginUser(
-                      email: _emailController.text,
-                      password: _passwordController.text);
-                  if (result == "Successfully Logged-In") {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => CounterScreen()));
-                  } else {
-                    showSnackBar(result, context);
-                  }
-                  setState(() {
-                    _isLoading = false;
-                  });
-                },
-                child: _isLoading
-                    ? SizedBox(
-                        height: 52,
-                        child: Lottie.asset("lib/assets/images/Loading.json"))
-                    : AnimatedContainer(
-                        duration: const Duration(milliseconds: 200),
-                        height: 50,
-                        width: deviceWidth * 0.5,
-                        decoration: BoxDecoration(
-                            color: Colors.teal.shade400,
-                            borderRadius: BorderRadius.circular(50),
-                            boxShadow: _isElevated
-                                ? [
-                                    BoxShadow(
-                                        color: Colors.teal.shade800,
-                                        offset: const Offset(4, 4),
-                                        blurRadius: 15,
-                                        spreadRadius: 1),
-                                    const BoxShadow(
-                                        color: Colors.white,
-                                        offset: Offset(-4, -4),
-                                        blurRadius: 15,
-                                        spreadRadius: 1)
-                                  ]
-                                : null),
-                        child: Center(
-                          child: Text("Login",
-                              style: GoogleFonts.cagliostro(
-                                  fontSize: 22, fontWeight: FontWeight.bold)),
-                        ),
-                      ),
-              ),
-              //Flexible Widget
-              Flexible(
-                child: Container(),
-                flex: 3,
-              ),
-              //Transtioning for signup
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    child: const Text("Don't you have an account?"),
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  GestureDetector(
-                    onTap: () {
+          height: deviceHeight,
+          width: deviceWidth,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: deviceHeight * 0.09),
+                  child: Lottie.asset(
+                      //Lottie Animation
+                      "lib/assets/images/signIn.json"),
+                ),
+                //SizedBox
+                const Opacity(opacity: 0.0, child: Divider()),
+                //Welcome Message
+                Text("Welcome",
+                    style: GoogleFonts.cagliostro(
+                        fontSize: MediaQuery.of(context)
+                            .textScaleFactor
+                            .clamp(deviceWidth * 0.101, deviceWidth * 0.2),
+                        fontWeight: FontWeight.w600)),
+                //SizedBox
+                const Opacity(opacity: 0.0, child: Divider()),
+                //InputFormField for email
+                TextFieldInput(
+                    textEditingController: _emailController,
+                    hintText: "Enter your Username",
+                    labelText: "Username",
+                    textInputType: TextInputType.emailAddress),
+                //SizedBox
+                const SizedBox(
+                  height: 20,
+                ),
+                //InputFormfield for password
+                TextFieldInput(
+                  textEditingController: _passwordController,
+                  hintText: "Enter your Password",
+                  labelText: "Password",
+                  textInputType: TextInputType.text,
+                  isPass: true,
+                ),
+                //FlexibleWidget
+                const Opacity(opacity: 0.0, child: Divider()),
+                //Login Button
+                GestureDetector(
+                  onTap: () async {
+                    setState(() {
+                      _isLoading = true;
+                      _isElevated = !_isElevated;
+                    });
+                    await Future.delayed(const Duration(milliseconds: 400));
+                    String result = await AuthMethods().loginUser(
+                        email: _emailController.text,
+                        password: _passwordController.text);
+                    if (result == "Successfully Logged-In") {
                       Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => const SignUpScreen()));
-                    },
-                    child: Container(
-                      child: const Text(
-                        " Sign Up.",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
+                              builder: (context) => CounterScreen()));
+                    } else {
+                      showSnackBar(result, context);
+                    }
+                    setState(() {
+                      _isLoading = false;
+                    });
+                  },
+                  child: _isLoading
+                      ? SizedBox(
+                          height: 52,
+                          child: Lottie.asset("lib/assets/images/Loading.json"))
+                      : AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          height: 50,
+                          width: deviceWidth * 0.5,
+                          decoration: BoxDecoration(
+                              color: Colors.teal.shade400,
+                              borderRadius: BorderRadius.circular(50),
+                              boxShadow: _isElevated
+                                  ? [
+                                      BoxShadow(
+                                          color: Colors.teal.shade800,
+                                          offset: const Offset(4, 4),
+                                          blurRadius: 15,
+                                          spreadRadius: 1),
+                                      const BoxShadow(
+                                          color: Colors.white,
+                                          offset: Offset(-4, -4),
+                                          blurRadius: 15,
+                                          spreadRadius: 1)
+                                    ]
+                                  : null),
+                          child: Center(
+                            child: Text("Login",
+                                style: GoogleFonts.cagliostro(
+                                    fontSize: 22, fontWeight: FontWeight.bold)),
+                          ),
+                        ),
+                ),
+                //Flexible Widget
+                const Opacity(opacity: 0.0, child: Divider()),
+                //Transtioning for signup
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: const Text("Don't you have an account?"),
                       padding: const EdgeInsets.symmetric(vertical: 8),
                     ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 10)
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUpScreen()));
+                      },
+                      child: Container(
+                        child: const Text(
+                          " Sign Up.",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10)
+              ],
+            ),
           ),
         )));
   }
